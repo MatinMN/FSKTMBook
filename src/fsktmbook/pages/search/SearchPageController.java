66bed19b6755dd6196b3f233d74c7c4d5d9825cc
azraf019 <azraf019@gmail.com>
2018-12-17 20:20:42 +0800

    fsktm search page

---
+++ src/fsktmbook/pages/search/SearchPageController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fsktmbook.pages.search;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Azraf
+ */
+public class SearchPageController implements Initializable {
+
+    @FXML
+    private TextField search_bar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void search(ActionEvent event) {
+    }
+    
+}
