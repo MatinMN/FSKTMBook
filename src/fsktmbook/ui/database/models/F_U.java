18836a267384bc8e3b8dfa3afee8d5e3fdd18ad1
admin <admin@192.168.43.108>
2018-12-18 22:52:36 +0800

    function to follow and unfollow. Sorry, but the name had to be changed.

---
+++ src/fsktmbook/ui/database/models/F_U.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fsktmbook.ui.database.models;
+
+import fsktmbook.helpers.Helper;
+import fsktmbook.helpers.Post;
+import fsktmbook.ui.database.Database;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Matin
+ */
+public class F_U {
+    
+    private Database database;
+    
+    
+    public void Follows(){
+        database = Database.getInstannce();
+    }
+    
+    
+        
+    public boolean follow(int followedId , int followerId) throws SQLException{
+
+        String query = "INSERT INTO follows (followerId,followedId,followDate) VALUES (?,?,?)";   
+                
+        PreparedStatement stmt = database.prepareStatement(query);
+        
+        stmt.setInt(1, followerId);
+        stmt.setInt(2, followedId);
+        stmt.setString(3, Helper.getCurrentTime());
+        
+        boolean res = stmt.execute();
+        
+        return res;
+        
+    }
+    
+    public boolean unfollow(int followedId, int followerId) throws SQLException{
+        String query = "DELETE FROM follows WHERE followerID='" + followerId + "' AND followedId='" + followedId + "'";
+        PreparedStatement stmt = database.prepareStatement(query);
+        
+        return (stmt.execute());
+    }
+    
+    
+    
+    
+}
